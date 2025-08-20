044fbded5a84573dbe96f3bf6e487561dcc4d235
Nadhirafaza <138868247+Nadhirafaza@users.noreply.github.com>
2025-08-20 22:43:12 +0700

    Update siponinona.py

--- siponinona.py
+++ siponinona.py
@@ -446,13 +446,12 @@ else:
             }
 
             df_koordinat = pd.DataFrame(koordinat_data)
-            df = df.merge(df_koordinat, on="Kecamatan", how="left")
 
             # === Plot ke Peta ===
             fig = px.scatter_mapbox(
                 lat="Latitude",
                 lon="Longitude",
-                Color="Cluster"
+                color="Cluster",
                 hover_name="Kecamatan",
                 zoom=9,
                 height=600,
