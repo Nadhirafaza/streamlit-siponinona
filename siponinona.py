9bfb56e0f5830539629597939af766b14959f0eb
Nadhirafaza <138868247+Nadhirafaza@users.noreply.github.com>
2025-08-21 16:58:45 +0700

    Update siponinona.py

--- siponinona.py
+++ siponinona.py
@@ -502,7 +502,13 @@ else:
             3: '3 Armada'
             }
             
-            df_map['Cluster_Label'] = df_map['Cluster'].map(cluster_label_map)
+            symbol_map = {
+            '1 TPS3R': 'circle',
+            '2 Bank Sampah': 'square',
+            '3 Armada': 'diamond'
+            }
+
+            df_map['Marker_Symbol'] = df_map['Cluster_Label'].map(symbol_map)
             
             fig_map = px.scatter_mapbox(
             df_map,
@@ -511,10 +517,12 @@ else:
             hover_name="Nama Kecamatan",
             hover_data=["Cluster_Label", "Volume Sampah Tidak Terlayani"],
             color="Cluster_Label",
+            symbol="Marker_Symbol",
             zoom=10,
             height=600,
-            category_orders={'Cluster_Label': ['1 TPS3R', '2 Bank Sampah', '3 Armada']},  # urut sesuai keinginan
-            color_discrete_map={'1 TPS3R':'orange','2 Bank Sampah':'blue','3 Armada':'green'}
+            category_orders={'Cluster_Label': ['1 TPS3R', '2 Bank Sampah', '3 Armada']},
+            color_discrete_map={'1 TPS3R':'orange','2 Bank Sampah':'blue','3 Armada':'green'},
+            symbol_sequence=['circle', 'square', 'diamond']  # urutan simbol
             )
 
             fig_map.update_layout(
